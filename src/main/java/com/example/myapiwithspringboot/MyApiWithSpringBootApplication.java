07a09a3eb43fd256da333446c4db3e24ec6fd689
nbogie <neillbogie@googlemail.com>
2024-07-16 23:02:26 +0100

    initial

---
+++ src/main/java/com/example/myapiwithspringboot/MyApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapiwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApiWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myapiwithspringboot/MyApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myapiwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
